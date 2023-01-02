a389d92a6dc9d424e7c0e3af26f30d4abc8d2616
Arun Upadhyay <arunnnet@gmail.com>
2023-01-02 09:02:03 -0500

    Added default database of docker container run

--- docker-compose/mysql/init/01-databases.sql
+++ docker-compose/mysql/init/01-databases.sql
@@ -3,8 +3,8 @@ CREATE USER 'local_developer'@'db' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON *.* TO 'local_developer'@'%';
 
 # create databases
-CREATE DATABASE IF NOT EXISTS `local_laravel`;
-CREATE TABLE `local_laravel`.`game` (
+CREATE DATABASE IF NOT EXISTS `local_db`;
+CREATE TABLE `local_db`.`game` (
                                         id int NOT NULL AUTO_INCREMENT,
                                         title varchar(255) NOT NULL,
                                         rating varchar(255),
@@ -13,3 +13,5 @@ CREATE TABLE `local_laravel`.`game` (
                                         PRIMARY KEY (id)
 
 );
+
+INSERT into `local_db`.`game` (title, rating, review, last_played) VALUES ('Sample Game', '5','Outstanding','2022-12-21 00:00:00');
